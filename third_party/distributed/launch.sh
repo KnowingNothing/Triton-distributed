424e2ef70234480a1c5128912bd8e3e905b98b16
郑学贵 <zhengxuegui.0@bytedance.com>
2025-02-13 06:37:57 +0000

    [TritonDistributed] rank/num_ranks/symm_at dist ops and inline nvshmem ptx support
    * fix rebase
    
    
    * comment
    
    
    * format
    
    
    * clean
    
    
    * [TritonDistributed] rank/num_ranks/symm_at dist ops and inline nvshmem ptx support
    
    
    See merge request: !7

---
+++ third_party/distributed/launch.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export CUDA_LAUNCH_BLOCKING=0
+export TORCH_CPP_LOG_LEVEL=1
+export NCCL_DEBUG=ERROR
+
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+DISTRIBUTED_DIR=$SCRIPT_DIR
+THIRD_PARTY_DIR=$(dirname -- "$SCRIPT_DIR")
+
+TRITON_NVSHMEM_DIR=${THIRD_PARTY_DIR}/nvshmem_bind/python
+PYNVSHMEM_DIR=${THIRD_PARTY_DIR}/nvshmem_bind/pynvshmem
+NVSHMEM_ROOT=${THIRD_PARTY_DIR}/nvshmem/build/install
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${NVSHMEM_ROOT}/lib
+export NVSHMEM_DISABLE_CUDA_VMM=1 # moving from cpp to shell
+export NVSHMEM_BOOTSTRAP=UID
+
+export PYTHONPATH=$PYTHONPATH:${PYNVSHMEM_DIR}/build:$TRITON_NVSHMEM_DIR
+export NVSHMEM_BOOTSTRAP_UID_SOCK_IFNAME=eth0
+
+export PYTHONPATH=$PYTHONPATH:${TRITON_NVSHMEM_DIR}:${PYNVSHMEM_DIR}/build
+
+export TRITON_CACHE_DIR=$DISTRIBUTED_DIR/.triton_cache
+export NVSHMEM_HOME=${NVSHMEM_ROOT}
+mkdir -p $DISTRIBUTED_DIR/.triton_cache
+
+nproc_per_node=${ARNOLD_WORKER_GPU:=$(nvidia-smi --list-gpus | wc -l)}
+nnodes=${ARNOLD_WORKER_NUM:=1}
+node_rank=${ARNOLD_ID:=0}
+
+master_addr=${ARNOLD_WORKER_0_HOST:="127.0.0.1"}
+if [ -z ${ARNOLD_WORKER_0_PORT} ]; then
+  master_port="23456"
+else
+  master_port=$(echo "$ARNOLD_WORKER_0_PORT" | cut -d "," -f 1)
+fi
+
+additional_args="--rdzv_endpoint=${master_addr}:${master_port}"
+
+CMD="torchrun \
+  --node_rank=${node_rank} \
+  --nproc_per_node=${nproc_per_node} \
+  --nnodes=${nnodes} \
+  ${additional_args} \
+  $@"
+
+echo ${CMD}
+${CMD}
+
+ret=$?
+exit $ret
