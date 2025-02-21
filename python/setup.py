ee2fadcef081d45c2a0a0ce4acbdd647b3fc7db6
黄晨晖 <huangchenhui.yellow@bytedance.com>
2025-02-21 18:26:25 +0000

    feat: download pre-compiled llvm from tos
    * chore
    
    
    * feat: download pre-compiled llvm from tos
    
    
    See merge request: !23

--- python/setup.py
+++ python/setup.py
@@ -223,7 +223,7 @@ def get_llvm_package_info():
     name = f"llvm-{rev}-{system_suffix}"
     # Create a stable symlink that doesn't include revision
     sym_name = f"llvm-{system_suffix}"
-    url = f"https://oaitriton.blob.core.windows.net/public/llvm-builds/{name}.tar.gz"
+    url = f"http://tosv.byted.org/obj/turing/dist_triton/{name}.tar.gz"
     return Package("llvm", name, url, "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH", sym_name=sym_name)
 
 
