a81469eb9502704aec0e5d48ee4ac302c14df34e
李天祺 <litianqi.03@bytedance.com>
2025-03-21 07:48:39 +0000

    feat: Add intra node MoE+RS TP op with notify and wait
    * feat: add intra node moe tp group gemm rs op with notify and wait
    
    
    * feat: add intra node moe tp group gemm rs op with notify and wait
    
    
    * feat: add intra node moe rs tp op with notify and wait
    
    
    * Fix atomic_add constexpr bug.
    
    
    * Add CI test
    
    
    * Use notify and wait ops for intra node moe_rs.
    
    
    * Add intra node moe_rs tp support
    
    
    See merge request: !52

--- third_party/distributed/distributed/kernels/nvidia/__init__.py
+++ third_party/distributed/distributed/kernels/nvidia/__init__.py
@@ -28,11 +28,26 @@ from .flash_decode import (gqa_fwd_batch_decode_persistent, kernel_gqa_fwd_batch
                            gqa_fwd_batch_decode_persistent_aot, gqa_fwd_batch_decode, gqa_fwd_batch_decode_aot,
                            gqa_fwd_batch_decode_intra_rank, kernel_inter_rank_gqa_fwd_batch_decode_combine_kv)
 from .gemm_reduce_scatter import create_gemm_rs_context, gemm_rs_multi_node
+from .moe_reduce_rs import create_moe_rs_context, get_dataflowconfig, moe_reduce_rs_intra_node
 
 __all__ = [
-    "ag_gemm_intra_node", "create_ag_gemm_intra_node_context", "gqa_fwd_batch_decode_persistent",
-    "kernel_gqa_fwd_batch_decode_split_kv_persistent", "gqa_fwd_batch_decode_persistent_aot", "gqa_fwd_batch_decode",
-    "gqa_fwd_batch_decode_aot", "gqa_fwd_batch_decode_intra_rank", "kernel_inter_rank_gqa_fwd_batch_decode_combine_kv",
-    "fast_allgather", "create_fast_allgather_context", "gemm_rs_multi_node", "create_gemm_rs_context",
-    "_forward_pull_kernel", "_forward_push_2d_kernel", "_forward_push_2d_ll_kernel"
+    "ag_gemm_intra_node",
+    "create_ag_gemm_intra_node_context",
+    "gqa_fwd_batch_decode_persistent",
+    "kernel_gqa_fwd_batch_decode_split_kv_persistent",
+    "gqa_fwd_batch_decode_persistent_aot",
+    "gqa_fwd_batch_decode",
+    "gqa_fwd_batch_decode_aot",
+    "gqa_fwd_batch_decode_intra_rank",
+    "kernel_inter_rank_gqa_fwd_batch_decode_combine_kv",
+    "fast_allgather",
+    "create_fast_allgather_context",
+    "gemm_rs_multi_node",
+    "create_gemm_rs_context",
+    "_forward_pull_kernel",
+    "_forward_push_2d_kernel",
+    "_forward_push_2d_ll_kernel",
+    "create_moe_rs_context",
+    "get_dataflowconfig",
+    "moe_reduce_rs_intra_node",
 ]
